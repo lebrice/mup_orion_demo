369bc70d7d9f076088f0eb77191947eebf73a50d
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-10-20 18:22:07 -0400

    fix tiny typo
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- mup_demo/trainer_example.py
+++ mup_demo/trainer_example.py
@@ -23,7 +23,7 @@ https://huggingface.co/models?filter=text-generation
 Example command:
 ```
 accelerate launch mup_demo/trainer_example.py \
-    --model_name_or_path gpt2 --dataset_name wikitext \ 
+    --model_name_or_path gpt2 --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
     --per_device_trai:n_batch_size 8 --per_device_eval_batch_size 8 \
     --do_train --do_eval --output_dir test-clm2 \
