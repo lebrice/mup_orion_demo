c033f28c6d41924bb7056d69420516a42686fb9b
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-11-03 15:16:32 -0400

    Update the experiment config, increase max_broken
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- run_sweep.sh
+++ run_sweep.sh
@@ -19,7 +19,7 @@ export WANDB_WATCH=all
 export WANDB_PROJECT=mup_demo
 export WANDB_TAGS=$EXP_NAME
 
-orion hunt -n $EXP_NAME --config sweep_config.yaml --exp-max-trials=50 --working-dir runs/$EXP_NAME \
+orion hunt -n $EXP_NAME --config sweep_config.yaml --working-dir runs/$EXP_NAME \
     accelerate launch mup_demo/train.py \
     --output_dir {exp.working_dir}/{trial.id} --overwrite_output_dir=True \
     --run_name {exp.name}-{trial.id} \
