15d930eaa06547bfcbb74ee55e8f55c0201a4bb5
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-11-08 14:32:18 -0500

    Tweaking the train.sh script to work multi-node
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- train.sh
+++ train.sh
@@ -13,16 +13,27 @@ conda activate $SCRATCH/conda/mup
 EXP_NAME="gpt2_256"
 
 export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+export WORLD_SIZE=$((${SLURM_JOB_NUM_NODES} * ${SLURM_NTASKS_PER_NODE}))
 export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
+echo "MASTER_ADDR: $MASTER_ADDR"
+echo "MASTER_PORT: $MASTER_PORT"
+echo "WORLD_SIZE: $WORLD_SIZE"
+echo "NODE RANK: $SLURM_NODEID"
+echo "SLURM_CPUS_ON_NODE: $SLURM_CPUS_ON_NODE"
 # Optional: Set some wandb-related environment variables.
-# export WANDB_LOG_MODEL=1
-# export WANDB_WATCH=all
+#export WANDB_LOG_MODEL=1
+#export WANDB_WATCH=all
 export WANDB_PROJECT=mup_demo
 export WANDB_TAGS=$EXP_NAME
 
+
+echo "Running mup_demo/train.py $@"
+# Actually start experiment. NUM_GPUS is an environment variable set in trigger script
+#torchrun --node_rank $SLURM_NODEID --nnodes $SLURM_JOB_NUM_NODES --nproc_per_node=$SLURM_GPUS_ON_NODE --standalone mup_demo/train.py "$@"
+
 accelerate launch \
-    --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT \
-    --num_machines $SLURM_NNODES --num_processes $WORLD_SIZE \
-    mup_demo/train.py "$@"
+     --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT --num_cpu_threads_per_process=$SLURM_CPUS_ON_NODE \
+     --num_machines $SLURM_NNODES --num_processes $WORLD_SIZE --machine_rank=$SLURM_NODEID \
+     mup_demo/train.py "$@"
